84950d2095cf9b5f301770bdfd07e1d52ff19b4a
Abou DIOP <abou.diop.98@gmail.com>
2017-02-14 21:53:06 +0000

    All templates done and work well

--- resto_app.py
+++ resto_app.py
@@ -11,47 +11,82 @@ session = DBSession()
 
 app = Flask(__name__)
 
+
+# Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+
+# Fake Menu Items
+items = []
+items = [
+ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'},
+ {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},
+ {'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},
+ {'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},
+ {'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'}
+  ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
+
 #Home wep App list All restaurants
 @app.route('/')
 @app.route('/restaurants')
 def home():
-    return "Home here"
+    restaurants = session.query(Restaurant).all()
+    return render_template('index.html', restaurants = restaurants)
+
+
 
 # Add a new Restaurant
-@app.route('/restaurant/new')
+@app.route('/restaurant/new', methods = ['GET', 'POST'])
 def newRestaurant():
-    return "Create new Restautant"
+    return render_template('newRestaurant.html')
+
+
 
 # Edit a Restaurant @params id restaurant
-@app.route('/restaurant/<int:restaurant_id>/edit')
+@app.route('/restaurant/<int:restaurant_id>/edit', methods = ['GET', 'POST'])
 def editRestaurant(restaurant_id):
-    return "Edit restaurant"
+    return render_template('editRestaurant.html', restaurant = restaurant)
+
+
 
 # Delete a restaurant @params id restaurant
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-    return "Delete Restaurant "
+    return render_template('deleteRestaurant.html', restaurant = restaurant)
+
+
 
 # Show menu of restaurant @params id restaurant
 @app.route('/restaurant/<int:restaurant_id>/menu')
 @app.route('/restaurant/<int:restaurant_id>')
-
 def showMenuItem(restaurant_id):
-    return "Show menu of restaurant"
+    return render_template('showMenuItem.html', restaurant = restaurant, items = items)
+
+
+
 #new menu item
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
-    return "Add a new menu item"
+    return render_template('newMenuItem.html')
+
+
 
 #edit menu item
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-    return "Edit menu item"
+    return render_template('editMenuItem.html', item = item)
+
+
 
 # delete a menu item
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
-def deleteMenuITem(restaurant_id, menu_id):
-    return "Delete menu item"
+def deleteMenuItem(restaurant_id, menu_id):
+    return render_template('deleteMenuItem.html', item = item)
+
 
 
 if __name__ == '__main__':
